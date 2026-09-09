03e8c4c0c136659eb74577ee8efd269b16e16664
bobo liu <liu@bobo.moe>
2026-09-09 22:03:52 +0800

    fix(launch): match IsGBKEncoding behavior with Framework

--- PCL.Core/Utils/OS/SystemInfo.cs
+++ PCL.Core/Utils/OS/SystemInfo.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace PCL.Core.Utils.OS;
 
@@ -19,7 +18,7 @@ public static class SystemInfo
     /// <summary>
     /// 是否使用 GBK 编码。
     /// </summary>
-    public static readonly bool IsGBKEncoding = Encoding.Default.CodePage == 936;
+    public static readonly bool IsGBKEncoding = KernelInterop.GetACP() == 936;
 
     /// <summary>
     /// 系统信息描述，例如 Microsoft Windows 11 专业工作站版 10.0.22635.0
